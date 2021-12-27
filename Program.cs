3b345897e3b584d2ef928ef125749ab236eeef21
Dmitry Anoschenkov <danoschenkov@gmail.com>
2021-12-27 21:45:13 +0300

    Function for selecting even elements created

--- Program.cs
+++ Program.cs
@@ -20,12 +20,27 @@ void PrintArray(int[] Array2)
     }
 }
 
-void SelectEvenElements(int[] ArrayInitial, int[]ArrayResult)
+int SelectEvenElements(int[] ArrayInitial, int[] ArrayResult)
 // Функция выбирает четные элементы из изначального массива и копирует их в конечный массив
 {
+    int j = 0;
+
     for (int i = 0; i < ArrayInitial.Length; i++)
     {
-        if(ArrayInitial[i] % 2 == 0)
+            if(ArrayInitial[i] % 2 == 0)
+            {
+            ArrayResult[j] = ArrayInitial[i];
+            j++;
+            return ArrayResult[j];
+            }
     }
 }
+Console.Clear();
 
+int[] ArrayInitial = new int[10];
+int[] ArrayResult = new int[ArrayResult.Length];
+
+FillArray(ArrayInitial);
+PrintArray(ArrayInitial);
+SelectEvenElements(ArrayInitial, ArrayResult);
+PrintArray(ArrayResult);
